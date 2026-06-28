905641d91fd92d557c7df74547bdf16d7fcb7ed7
shock1824 <100500297+shock1824@users.noreply.github.com>
2026-06-28 19:08:49 +0800

    Update compressor_script.bat

--- compressor_script.bat
+++ compressor_script.bat
@@ -1,22 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
-#set INPUT to a file directory instead of a folder to process one file and folder directory to process multiple files automatically.
+#set INPUT to a folder location instead of a file directory to process one file and folder directory to process multiple files automatically.
 
 
-set "INPUT="
-set "KEY="
-set "WORK="
-set "OUTPUT="
-set "DIRECTORY="
-#Directory where the command is located.
-set "CPU="
-set "threads="
+set KEY=
+set INPUT=
+set WORK=
+set OUTPUT=
+set DIRECTORY=
+#Directory where the mai2dat is located.
+set CPU=
+set threads=
+set compression rate=
 
 uvx wannacri extractusm %INPUT% --key %KEY% -o %WORK%
 for /r %WORK% %%f in (*.ivf) do (
     for /f "tokens=2 delims== " %%a in ('ffprobe -v 0 -select_streams v:0 -show_entries format^=bit_rate -of default^=noprint_wrappers^=1 "%%f"') do (
-        set /a target=%%a/6
+        set /a target=%%a/compression rate%
         set /a buffer=!target!*2
 
         ffmpeg -i "%%f" -r 30 -c:v libvpx-vp9 
